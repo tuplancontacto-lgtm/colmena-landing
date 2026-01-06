3ed8fb14853cb899c0319e6c527276be355dc7a5
tuplancontacto-lgtm <tuplancontacto@gmail.com>
2026-01-06 13:07:29 -0300

    Update data.js
    Chao

--- Api/data.js
+++ Api/data.js
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-git mv api.json data.js
-export default function handler(req, res) {
-  res.status(200).json({ mensaje: 'API funcionando' });
-}
-</script>
 
-</body>
-</html>
