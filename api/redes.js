5789dd5bf12f0feaec6cff494c715554db469fdc
tuplancontacto-lgtm <tuplancontacto@gmail.com>
2026-01-06 16:14:08 -0300

    Update redes.js
    jgjsd

--- api/redes.js
+++ api/redes.js
@@ -1,1 +1,17 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    "COLMENA MAX": {
+      "zonas": ["NORTE", "CENTRO", "SUR", "NACIONAL"],
+      "grupos": ["G41","G42","G43","G44","G45","G46","GS1","GS2","GS3","GS4"]
+    },
+    "COLMENA MAX SUR": {
+      "zonas": ["SUR"],
+      "grupos": ["GS1","GS2","GS3","GS4"]
+    },
+    "COLMENA PLUS": {
+      "zonas": ["CENTRO"],
+      "grupos": ["G41","G42","G43","G44","G45","G46"]
+    }
+  });
+}
 
