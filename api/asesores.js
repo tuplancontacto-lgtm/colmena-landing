1b55b3bf954da0fb42aa74cb289c14e57d6c47a5
tuplancontacto-lgtm <tuplancontacto@gmail.com>
2026-01-07 15:37:36 -0300

    Update asesores.js
    edita tu archivo api/asesores.js. Cambia esto:
    export default function handler(req, res) {

--- api/asesores.js
+++ api/asesores.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   // Habilitar CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
